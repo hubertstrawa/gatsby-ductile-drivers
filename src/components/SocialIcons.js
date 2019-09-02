cd222dc353821163063f9a9f78c95fe121e303f3
Vinyljam <hubertstrawa@gmail.com>
2019-09-03 01:22:43 +0200

    adding links

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import facebookIcon from '../assets/icons/facebook.svg';
-import twitterIcon from '../assets/icons/twitter.svg';
-import instagramIcon from '../assets/icons/instagram.svg';
-import spotifyIcon from '../assets/icons/spotify.svg';
-import youtubeIcon from '../assets/icons/youtube.svg';
-import appleincIcon from '../assets/icons/appleinc.svg';
-import socialStyles from './SocialIcons.module.scss'
+import React from "react"
+import facebookIcon from "../assets/icons/facebook.svg"
+import twitterIcon from "../assets/icons/twitter.svg"
+import instagramIcon from "../assets/icons/instagram.svg"
+import spotifyIcon from "../assets/icons/spotify.svg"
+import youtubeIcon from "../assets/icons/youtube.svg"
+import appleincIcon from "../assets/icons/appleinc.svg"
+import socialStyles from "./SocialIcons.module.scss"
 
 const SocialIcons = () => {
-
   return (
     <div className={socialStyles.wrapper}>
-      <img className={socialStyles.icon} src={facebookIcon} />
+      <a href="https://www.facebook.com/ductiledrivers/" target="_blank">
+        <img className={socialStyles.icon} src={facebookIcon} />
+      </a>
       <img className={socialStyles.icon} src={twitterIcon} />
-      <img className={socialStyles.icon} src={instagramIcon} />
+      <a href="https://www.instagram.com/ductiledrivers/" target="_blank">
+        <img className={socialStyles.icon} src={instagramIcon} />
+      </a>
       <img className={socialStyles.icon} src={youtubeIcon} />
-      <img className={socialStyles.icon} src={spotifyIcon} />
+      <a
+        href="https://open.spotify.com/artist/0jO5u6hVZiwr0GKu3H4ywQ/"
+        target="_blank"
+      >
+        <img className={socialStyles.icon} src={spotifyIcon} />
+      </a>
       <img className={socialStyles.icon} src={appleincIcon} />
     </div>
-  );
-};
+  )
+}
 
-export default SocialIcons;
+export default SocialIcons
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,13 @@ import SocialIcons from '../components/SocialIcons'
 import albumCover from '../assets/cover.png'
 
 const IndexPage = () => {
+
+    let arr = [];
     return (
         <Layout>
             <div className={indexStyles.wrapper}>
                 <img className={indexStyles.album} src={albumCover} alt="Ductile Drivers album cover"/>
-                <a className={indexStyles.btn} href="#">Listen</a>
+                <a className={indexStyles.btn} href="https://distrokid.com/hyperfollow/ductiledrivers/lack" target="_blank">Listen</a>
                 <SocialIcons />
             </div>
         </Layout>
